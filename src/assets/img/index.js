12f0ca748eb5a3dbe9868ab281f2d7a8ecf63b10
Kushbak Mamytov <mamytovkushbak@gmail.com>
2024-03-20 05:24:16 +0600

    add images and icons

---
+++ src/assets/img/index.js
@@ -0,0 +1,33 @@
+import arrowBottom from './icons/arrow-bottom.svg'
+import arrowTop from './icons/arrow-top.svg'
+import bookmark from './icons/bookmark.svg'
+import clock from './icons/clock.svg'
+import comment from './icons/comment.svg'
+import edit from './icons/edit.svg'
+import image from './icons/image.svg'
+import play from './icons/play.svg'
+import star from './icons/star.svg'
+import trash from './icons/trash.svg'
+import triangleBottom from './icons/triangle-bottom.svg'
+import video from './icons/video.svg'
+import xMark from './icons/x-mark.svg'
+
+import loadImg from './load-img.png'
+
+// Export the imports
+export {
+  loadImg,
+  arrowBottom,
+  arrowTop,
+  bookmark,
+  clock,
+  comment,
+  edit,
+  image,
+  play,
+  star,
+  trash,
+  triangleBottom,
+  video,
+  xMark,
+}
