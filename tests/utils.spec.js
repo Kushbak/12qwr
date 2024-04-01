fcf1e6ada0cb5b82e5b3e13a4829230e97c44144
Kushbak Mamytov <mamytovkushbak@gmail.com>
2024-04-01 06:31:23 +0600

    add needs to test and change test

--- tests/utils.spec.js
+++ tests/utils.spec.js
@@ -2,21 +2,18 @@ import { formatCommentDate } from '../src/utils'
 import { describe, expect, test } from 'vitest'
 
 describe('formatCommentDate', () => {
-  test('should format date correctly', () => {
-    const dateString = '2024-03-29T10:30:00Z'
-    const expectedFormattedDate = '29.03.2024 в 16:30'
+  test('should format date correctly with a specific time zone', () => {
+    const dateString = '2024-03-29T10:30:00Z' // Время в UTC
+    const expectedFormattedDate = '29.03.2024 в 16:30' // Ожидаемый вывод в часовом поясе GMT+5 (Например, Московское время)
+
+    const originalTimeZone = process.env.TZ
+
+    process.env.TZ = 'Europe/Moscow'
 
     const formattedDate = formatCommentDate(dateString)
 
     expect(formattedDate).toBe(expectedFormattedDate)
-  })
 
-  test('should handle leading zeros correctly', () => {
-    const dateString = '2024-01-05T09:05:00Z'
-    const expectedFormattedDate = '05.01.2024 в 15:05'
-
-    const formattedDate = formatCommentDate(dateString)
-
-    expect(formattedDate).toBe(expectedFormattedDate)
+    process.env.TZ = originalTimeZone
   })
 })
